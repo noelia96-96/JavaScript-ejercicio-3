8116628ac5546274b29a96831435d9f4b002e254
noelia96-96 <noeliagalindo96@gmail.com>
2020-11-11 14:38:32 +0100

    primer commit

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // Dicha la lista la obtendréis del documento "users.json" haciendo uso de la función "fetch". Una vez tengáis el array de usuarios, debéis generar otro array con la función "filter" para que solo estén los usuarios que tengán más de 500 en la propiedad "money".
 // Teniendo la lista ya filtrada, la recorreréis con el método "forEach" para mostrar por consola el nombre de cada usuario.
 
+//uso de fetch
+
 let buttonShowList = document.getElementById("showList");
 buttonShowList.addEventListener("click", getUsers);
 

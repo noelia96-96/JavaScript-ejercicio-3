395f63720cc4064d9335b7b1af1534c8f9c98ae4
noelia96-96 <noeliagalindo96@gmail.com>
2020-11-11 13:12:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bserrang-ejercicio-3-z2zsks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bserrang-ejercicio-3-z2zsks)
---
+++ script.js
@@ -0,0 +1,8 @@
+// En el siguiente ejercicio debéis sacar por consola (console.log) una lista con el nombre de los usuarios.
+// Dicha la lista la obtendréis del documento "users.json" haciendo uso de la función "fetch". Una vez tengáis el array de usuarios, debéis generar otro array con la función "filter" para que solo estén los usuarios que tengán más de 500 en la propiedad "money".
+// Teniendo la lista ya filtrada, la recorreréis con el método "forEach" para mostrar por consola el nombre de cada usuario.
+
+let buttonShowList = document.getElementById("showList");
+buttonShowList.addEventListener("click", getUsers);
+
+function getUsers() {}
